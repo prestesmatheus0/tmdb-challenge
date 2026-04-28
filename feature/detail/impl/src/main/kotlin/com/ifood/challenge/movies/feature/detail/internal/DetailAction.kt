f0826df370f76efe6ddda67a0653f0df4fcc7daa
prestesmatheus0 <barros.matheusprestes@gmail.com>
2026-04-28 12:45:39 -0300

    fix: ajuste lint blank-line-before-declaration em Action sealed interfaces

--- feature/detail/impl/src/main/kotlin/com/ifood/challenge/movies/feature/detail/internal/DetailAction.kt
+++ feature/detail/impl/src/main/kotlin/com/ifood/challenge/movies/feature/detail/internal/DetailAction.kt
@@ -2,5 +2,6 @@ package com.ifood.challenge.movies.feature.detail.internal
 
 internal sealed interface DetailAction {
     data object FavoriteToggle : DetailAction
+
     data object Retry : DetailAction
 }
--- feature/home/impl/src/main/kotlin/com/ifood/challenge/movies/feature/home/internal/HomeAction.kt
+++ feature/home/impl/src/main/kotlin/com/ifood/challenge/movies/feature/home/internal/HomeAction.kt
@@ -4,8 +4,12 @@ import com.ifood.challenge.movies.domain.movies.model.Movie
 
 internal sealed interface HomeAction {
     data class FilterSelect(val filter: HomeFilter) : HomeAction
+
     data class SearchQueryChange(val query: String) : HomeAction
+
     data object SearchToggle : HomeAction
+
     data class FavoriteToggle(val movie: Movie) : HomeAction
+
     data object Shuffle : HomeAction
 }
